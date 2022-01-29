22fe9a5024434d0be96ea029cf3ab968d7b2d3e7
Francis Githae <francis.githae@student.moringaschool.com>
2022-01-29 05:45:59 +0300

    fix flask_script error

--- manage.py
+++ manage.py
@@ -1,12 +1,12 @@
 from app import create_app
 # from flask_script._compat import text_type
-from flask_script import Manager,Server
+# from flask_script import Manager,Server
 
 app=create_app('development')
 
-manager=Manager(app)
-manager.add_command('server',Server)
-@manager.command
+# manager=Manager(app)
+# manager.add_command('server',Server)
+# @manager.command
 def test():
   """Run the unit tests"""
   import unittest
@@ -14,5 +14,8 @@ def test():
   unittest.TextTestRunner(verbosity=2).run(tests)
 
 
-if __name__=='__main__':
-  manager.run()
+# if __name__=='__main__':
+#   manager.run()
+
+if __name__ == '__main__':
+  app.run()
