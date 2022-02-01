bbd1188ad1856cdad82b26892de42c6c4b44f8c1
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-01 21:40:24 +0300

    define user and admin database models

---
+++ flasky.py
@@ -0,0 +1,15 @@
+import os
+from app import create_app
+from app import create_app,db
+from flask_migrate import Migrate
+from app.models import User,Role
+app=create_app('development')
+
+migrate=Migrate(app,db)
+
+@app.shell_context_processor
+def make_shell_context():
+  return dict(app=app,db=db,User=User,Role=Role)
+
+if __name__ == '__main__':
+  app.run()
--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 export MOVIE_API_KEY='48720892484f348bfcc60f5eeb524a7b'
 
 export SECRET_KEY='\xe5\x08\xcf`\xdan\xe7\xe2\t\x8d\xb7\xfd/N\xdb\x1e{\xfc`\xf2\x15\x1a8\x82'
-
-python3 manage.py server
+export FLASK_APP=flasky.py 
+# flask shell
+# flask run
