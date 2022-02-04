f53be8b877c3fe3cb7e634f38f8f9b303c1ebaf1
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-04 15:21:10 +0300

    fix start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 export MOVIE_API_KEY='48720892484f348bfcc60f5eeb524a7b'
 
 export SECRET_KEY='\xe5\x08\xcf`\xdan\xe7\xe2\t\x8d\xb7\xfd/N\xdb\x1e{\xfc`\xf2\x15\x1a8\x82'
-export FLASK_APP=flasky.py 
+export FLASK_APP=manage.py 
 
 
 # flask shell
-# flask run
+flask run
 # flask test
 # export FLASK_ENV=development
 #flask db init
