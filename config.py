da110a0370312ed2ddd49326a253f17f606c2f61
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-07 08:25:33 +0300

    set environment dev

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ class ProdConfig(Config):
   Args:
       Config:The parent configuration class with General configuration settings
   '''
-  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+  # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 class TestConfig(Config):
   SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:1234@localhost/watchlist_test'
